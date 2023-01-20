1e9e5b5ed4fd0d9f919296f9caf30a21768e2cbc
Rvjonh <jonhvelasco3@gmail.com>
2023-01-19 20:56:26 -0400

    create blog post function added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogListView, BLogDetailsView
+from .views import BlogListView, BLogDetailsView, BlogCreateView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("post/<int:pk>/", BLogDetailsView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
 ]
